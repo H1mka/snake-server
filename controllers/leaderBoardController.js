98367182976ec72d473ea9aebe29c12ffaee9a45
Adelev <kak-dela1488@ukr.net>
2023-11-02 11:38:05 +0200

    changeTableName

--- controllers/leaderBoardController.js
+++ controllers/leaderBoardController.js
@@ -6,7 +6,7 @@ class LeaderBoardController {
             const { player_name, player_score } = req.body;
 
             const newLeader = await db.query(
-                `INSERT INTO leader_board (player_name, player_score) values ($1, $2) RETURNING *`,
+                `INSERT INTO leaderboard (player_name, player_score) values ($1, $2) RETURNING *`,
                 [player_name, player_score]
             );
 
@@ -19,7 +19,7 @@ class LeaderBoardController {
 
     async getAllLeaders(req, res) {
         try {
-            const leaders = await db.query('SELECT * from leader_board');
+            const leaders = await db.query('SELECT * from leaderboard');
             const sortedLeaders = leaders.rows.sort((a, b) => b.player_score - a.player_score);
 
             res.status(200).json(sortedLeaders);
