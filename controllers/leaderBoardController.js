7e30d0f83f4294fedc78087a79298d1ff712e379
Adelev <kak-dela1488@ukr.net>
2023-11-07 14:35:57 +0200

    FixLeaderBoardBug

--- controllers/leaderBoardController.js
+++ controllers/leaderBoardController.js
@@ -5,16 +5,34 @@ class LeaderBoardController {
         try {
             const { player_name, player_score } = req.body;
 
-            const newLeader = await db.query(
-                `INSERT INTO leader_board (player_name, player_score) values ($1, $2) RETURNING *`,
-                [player_name, player_score]
-            );
+            const findLeader = await db.query('SELECT * FROM leader_board WHERE player_name = $1', [
+                player_name,
+            ]);
 
-            res.status(200).json(newLeader.rows);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: 'Internal server error' });
-        }
+            // Якщо такого гравця ще не існує, додаю гравця
+            if (!findLeader.rowCount) {
+                const newLeader = await db.query(
+                    `INSERT INTO leader_board (player_name, player_score) values ($1, $2) RETURNING *`,
+                    [player_name, player_score]
+                );
+
+                res.status(200).json(newLeader.rows);
+                return;
+            }
+
+            // Якщо вже існуючий гравець побив минулий рекорд
+            if (player_score > findLeader.rows[0].player_score) {
+                const updatePlayerScore = await db.query(
+                    'UPDATE leader_board SET player_score = $1 WHERE player_name = $2 RETURNING *',
+                    [player_score, player_name]
+                );
+
+                res.status(200).json(updatePlayerScore.rows);
+                return;
+            }
+
+            // Якщо новий рекорд нижче попереднього
+            res.status(200).json('New record lower than previous record');
     }
 
     async getAllLeaders(req, res) {
